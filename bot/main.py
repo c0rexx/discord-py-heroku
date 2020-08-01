65ecbede9ba12f4988b578a68992a8bfbadc51f9
c0rexx <69068849+c0rexx@users.noreply.github.com>
2020-08-01 10:00:46 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -292,7 +292,7 @@ async def fact(ctx, arg1: str = '', arg2: str = ''):
         await fact.add_reaction(random.choice(scoots_emoji))
 
 def img_to_text(image):
-    text = pytesseract.image_to_string(image)
+    text = pytesseract.image_to_string(image, lang='ara+ces+chi_tra+eng+rus+jpn+kor')
     if not text:
         text = 'HOW TO READ ' + pepega_emoji
     return text
