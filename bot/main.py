f71e8a20f37e1732c8d92cb551bbac299ba5897f
c0rexx <69068849+c0rexx@users.noreply.github.com>
2020-08-22 09:06:10 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -405,16 +405,24 @@ async def translate(ctx, *args):
     # Otherwise translate to english by default
     else:
         result = translator.translate(arg, dest='en')
-        
+    
+    msg = 'Translated from '
     # Chinese traditional (and possibly more languages) text does get recognized and translated, but isn't in googletrans.LANGUAGES (zh-CN), so this is a workaround
     lang_src = googletrans.LANGUAGES.get(result.src)
     lang_dst = googletrans.LANGUAGES.get(result.dest)
+    # Source language
     if lang_src is None:
-        lang_src = 'None (' + basic_emoji.get('Pepega') + basic_emoji.get('Clap') + ')'
+        msg += '`None` (' + basic_emoji.get('Pepega') + basic_emoji.get('Clap') + ')'
+    else:
+        msg += '`' + lang_src + '` '
+    msg += emoji_locale.code_to_country(result.src) + ' to '
+    # Destination language
     if lang_dst is None:
-        lang_dst = 'None (' + basic_emoji.get('Pepega') + basic_emoji.get('Clap') + ')'
-    # Send the translated text and info about origin and destination languages
-    msg = 'Translated from `' + lang_src + '` ' + emoji_locale.code_to_country(result.src) + ' to `' + lang_dst + '` ' + emoji_locale.code_to_country(result.dest) + '.'
+        msg += '`None` (' + basic_emoji.get('Pepega') + basic_emoji.get('Clap') + ')'
+    else:
+        msg += '`' + lang_dst + '` '
+        
+    msg += emoji_locale.code_to_country(result.dest) + '.'
     await ctx.send(msg + '\n```' + result.text[:1950] + '```')
 
 # https://stackoverflow.com/questions/56060614/how-to-make-a-discord-bot-play-youtube-audio
