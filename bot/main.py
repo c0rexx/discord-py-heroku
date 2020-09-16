9bcc5eddf63c04f5f3540af3250ed156dfb4afe6
c0rexx <69068849+c0rexx@users.noreply.github.com>
2020-09-16 13:02:38 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -804,7 +804,7 @@ async def ping(ctx):
     await ctx.send(basic_emoji.get('Pepega') + ' 🏓 Pong! `{0}ms`'.format(int(ms)))
     
 @bot.command(name='deth', aliases=['death'], help="Find out when you or someone else will die.")
-async def deth(ctx, user: Union[None, str, discord.User]):
+async def deth(ctx, user: Optional[str, discord.User]):
     # Set seed (consistent time everytime for each user)
     if not user:
         random.seed(ctx.message.author.id)
