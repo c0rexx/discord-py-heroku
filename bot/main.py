25ea168218db1b572c6afb596c01fe6431620543
LymanCZ <75006775+LymanCZ@users.noreply.github.com>
2020-11-30 08:32:50 +0100

    Bump
    Bump

--- bot/main.py
+++ bot/main.py
@@ -980,7 +980,7 @@ class Miscellaneous(commands.Cog):
         url_base = "http://stupidstuff.org/jokes/joke.htm?jokeid={}".format(id)
         
         joke_source = requests.get(url_base)
-        soup = BeautifulSoup(joke_source,'lxml')
+        soup = BeautifulSoup(joke_source,'html.parser')
         
         joke_body = soup.find('span',{'class':' gen_joke'})
         body = joke_body.text
